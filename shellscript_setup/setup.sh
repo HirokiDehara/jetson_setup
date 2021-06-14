5e5698f8d19d251bb15b017e2e67d6ab5e5a7e09
yutaro yamada <ymdyt.social@gmail.com>
2021-06-15 08:44:06 +0900

    setup.shを修正、testを追加

--- shellscript_setup/setup.sh
+++ shellscript_setup/setup.sh
@@ -3,22 +3,22 @@
 
 # 必須
 sudo apt update
-sudo apt install python3-pip build-essential
+sudo apt install python3-pip build-essential v4l-utils guvcview
 sudo pip3 install -U pip testresources setuptools==49.6.0 
 echo 'PATH="$PATH:$HOME/.local/bin"' >> ~/.bashrc
 echo 'LD_PRELOAD="/usr/lib/aarch64-linux-gnu/libgomp.so.1"' >> ~/.bashrc
+source ~/.bashrc
 
 # TensorFlowのインストール
 sudo apt install libhdf5-serial-dev hdf5-tools libhdf5-dev zlib1g-dev zip libjpeg8-dev liblapack-dev libblas-dev gfortran
 sudo pip3 install -U numpy==1.19.4 future==0.18.2 mock==3.0.5 h5py==2.10.0 keras_preprocessing==1.1.1 keras_applications==1.0.8 gast==0.2.2 futures protobuf pybind11
-## TensorFlow==2.3.1+nv20.12
+## TensorFlow==2.4.0+nv21.5
 sudo pip3 install --pre --extra-index-url https://developer.download.nvidia.com/compute/redist/jp/v45 tensorflow
 
 # PyTorchのインストール
 ## PyTorch==1.7.0
 wget https://nvidia.box.com/shared/static/cs3xn3td6sfgtene6jdvsxlr366m2dhq.whl -O torch-1.7.0-cp36-cp36m-linux_aarch64.whl
 sudo apt install libopenblas-base libopenmpi-dev
-pip3 install Cython
 pip3 install torch-1.7.0-cp36-cp36m-linux_aarch64.whl
 
 ## Torchvision==0.8.0
@@ -29,7 +29,10 @@ export BUILD_VERSION=0.8.0
 python3 setup.py install
 cd ..
 
-## yolov5に必要なパッケージのインストール
-pip3 install tqdm
-pip3 install seaborn
-pip3 install 'pillow<7'
+# YOLOv5に必要なパッケージのインストール
+sudo pip3 install -U matplotlib==3.2.2 scipy==1.4.1
+pip3 install Cython PyYAML==5.3.1  tqdm==4.41.1
+
+# テスト
+python3 test/tf_test.py
+python3 test/yolov5/detect.py --weights test/yolov5/weights/yolov5s.pt --source test/data/images/zidane.jpg --conf 0.25
