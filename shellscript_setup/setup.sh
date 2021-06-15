a05d8e810e67b144236a5dc382b62c7514a5e004
yutaro yamada <ymdyt.social@gmail.com>
2021-06-15 14:37:31 +0900

    引数の-yesを追加

--- shellscript_setup/setup.sh
+++ shellscript_setup/setup.sh
@@ -3,14 +3,15 @@
 
 # 必須
 sudo apt update
-sudo apt install python3-pip build-essential v4l-utils guvcview
+sudo apt upgrade -y 
+sudo apt install -y python3-pip build-essential v4l-utils guvcview
 sudo pip3 install -U pip testresources setuptools==49.6.0 
 echo 'PATH="$PATH:$HOME/.local/bin"' >> ~/.bashrc
 echo 'LD_PRELOAD="/usr/lib/aarch64-linux-gnu/libgomp.so.1"' >> ~/.bashrc
 source ~/.bashrc
 
 # TensorFlowのインストール
-sudo apt install libhdf5-serial-dev hdf5-tools libhdf5-dev zlib1g-dev zip libjpeg8-dev liblapack-dev libblas-dev gfortran
+sudo apt install -y libhdf5-serial-dev hdf5-tools libhdf5-dev zlib1g-dev zip libjpeg8-dev liblapack-dev libblas-dev gfortran
 sudo pip3 install -U numpy==1.19.4 future==0.18.2 mock==3.0.5 h5py==2.10.0 keras_preprocessing==1.1.1 keras_applications==1.0.8 gast==0.2.2 futures protobuf pybind11
 ## TensorFlow==2.4.0+nv21.5
 sudo pip3 install --pre --extra-index-url https://developer.download.nvidia.com/compute/redist/jp/v45 tensorflow
@@ -18,11 +19,11 @@ sudo pip3 install --pre --extra-index-url https://developer.download.nvidia.com/
 # PyTorchのインストール
 ## PyTorch==1.7.0
 wget https://nvidia.box.com/shared/static/cs3xn3td6sfgtene6jdvsxlr366m2dhq.whl -O torch-1.7.0-cp36-cp36m-linux_aarch64.whl
-sudo apt install libopenblas-base libopenmpi-dev
+sudo apt install -y libopenblas-base libopenmpi-dev
 pip3 install torch-1.7.0-cp36-cp36m-linux_aarch64.whl
 
 ## Torchvision==0.8.0
-sudo apt install libjpeg-dev zlib1g-dev libpython3-dev libavcodec-dev libavformat-dev libswscale-dev
+sudo apt install -y libjpeg-dev zlib1g-dev libpython3-dev libavcodec-dev libavformat-dev libswscale-dev
 git clone --branch v0.8.1 https://github.com/pytorch/vision torchvision
 cd torchvision
 export BUILD_VERSION=0.8.0
