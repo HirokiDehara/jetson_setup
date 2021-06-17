7d34f1598e662e0a1ebcaaa131680e9f3e406a03
YutaroYamada <60293562+YutaroYamada@users.noreply.github.com>
2021-06-17 11:11:28 +0900

    tf_testを伊藤くんのおすすめに差し替え

--- shellscript_setup/test/tf_test.py
+++ shellscript_setup/test/tf_test.py
@@ -1,5 +1,10 @@
+print('testing TensorFlow...')
 import tensorflow as tf
 
-hello = tf.constant('Hello World!')
-s = tf.Session()
-print(s.run(hello))
+print('TensorFlow version: ' + str(tf.__version__))
+print('Num GPUs Available: ', len(tf.config.experimental.list_physical_devices('GPU')))
+
+if str(tf.__version__).find("1.") == 0:
+	sess = tf.Session(config=tf.ConfigProto(log_device_placement=True))
+
+print('TensorFlow OK\n')
